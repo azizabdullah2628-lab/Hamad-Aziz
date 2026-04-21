b917df6e746defcff22c5f130384757103e9c129
azizabdullah2628-lab <azizabdullah2628@gmail.com>
2026-04-21 22:39:45 +0500

    lab task 7

---
+++ lab 02/LAB_2_TASK_7[1].cpp
@@ -0,0 +1,20 @@
+// Name: hammad aziz
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0715
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << " hammad aziz " << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
+
+
