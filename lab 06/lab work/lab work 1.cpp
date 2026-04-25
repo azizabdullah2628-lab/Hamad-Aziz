f102c605782144fc8d5710492afb4a8d4e98be85
azizabdullah2628-lab <azizabdullah2628@gmail.com>
2026-04-25 10:26:29 +0500

    Add files via upload

---
+++ lab 06/lab work/lab work 1.cpp
@@ -0,0 +1,15 @@
+//NAME:Hammad aziz
+//REG ID:BF25NWELE0715
+//DEPARTMENT:ELECTRICAL ENGINEERING (POWER)
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Loop from 1 to 10
+    for(int i = 1; i <=10; i++) {
+        // Display the number on a new line
+        cout << i << endl;
+    }
+
+    return 0;
+}
